48871d805560a3608d3abb02717178834a8a1cdc
strikegamer <strikegamer@gmail.com>
2013-05-02 02:42:43 -0300

    Created parameter to dont include sqlite3 gem in production on Heroku

--- app/models/checkin.rb
+++ app/models/checkin.rb
@@ -1,3 +1,5 @@
 class Checkin < ActiveRecord::Base
+  belongs_to :employee
+	validates_uniqueness_of :date, :scope => "employee_id"
   attr_accessible :date, :employee_id, :time1, :time2, :time3, :time4
 end
